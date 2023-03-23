4fea00d92dc05de314be12bc8cda28592b211d99
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-03-23 14:20:44 +0100

    can now see subtasks on tickets

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { DraggableProvided, DraggableStateSnapshot } from "react-beautiful-dnd";
 import { Ticket as TicketType } from "../../context/projects-data";
 import "./Ticket.scss";
@@ -10,6 +10,7 @@ type Props = {
 };
 
 export default function Ticket({ ticket, provided, snapshot }: Props) {
+    const [showSubtasks, setShowSubtasks] = useState(false);
     return (
         <div
             className="ticket"
@@ -18,10 +19,18 @@ export default function Ticket({ ticket, provided, snapshot }: Props) {
             {...provided.dragHandleProps}
             // @ts-ignore
             active={snapshot.isDragging.toString()}
+            onClick={() => setShowSubtasks(!showSubtasks)}
         >
             <h3>{ticket.title}</h3>
             <p>{ticket.description}</p>
             <p>{ticket.tasks.length + " Subtasks"}</p>
+            {showSubtasks && (
+                <ul>
+                    {ticket.tasks.map((task) => (
+                        <li>{task}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
